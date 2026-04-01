735f2e5a954752ce9f88a43410ee98d7f7499dde
subhashklvs <subhashklvs@gmail.com>
2026-04-01 12:01:27 +0530

    updated

---
+++ javascript/2.data.js
@@ -0,0 +1,108 @@
+let number = 20;
+number = 25;
+//let number = 20;
+console.log(number)
+const value = 30;
+//value = 32;
+//const value = 35;
+console.log(value);
+var sum = 40;
+sum= 45;
+var sum = 47
+console.log(sum);
+ 
+let addition = 6*6 + 12;
+console.log(addition,"sum")
+let subraction = 6*6 - 12;
+console.log(subraction)
+let multiplication = 6*12;
+console.log(multiplication)
+let division = 60/12;
+console.log(division)
+
+let b= 12;
+b-=3;
+console.log(b)
+let a= 12;
+a+=3;
+console.log(a)
+let c= 12;
+c*=3;
+console.log(c)
+let d= 12;
+d/=3;
+console.log(d)
+
+# Comparison Operators
+
+let number = 20;
+number = 25;
+//let number = 27;
+console.log(number);
+
+const value =30;
+//value = 35;
+//const value = 37;
+console.log(value);
+
+var sum = 40;
+sum = 45;
+var sum = 47;
+console.log(sum);
+
+let addition = 6* 6 + 12;
+console.log(addition, "sum");
+
+let substract = 45 - 20;
+console.log(substract);
+
+let product = 25 * 25;
+console.log(product);
+
+let division = 27/2;
+console.log(division);
+ 
+let remainder = 35 % 3;
+console.log(remainder);
+
+let a = 10;
+//a = a+20;
+a += 20;
+console.log(10);
+
+let b =12;
+b -= 3;
+console.log(b);
+
+let c=15;
+c*=4;
+console.log(c);
+
+let d=36;
+d /= 3;
+console.log(d);
+
+let e = 25;
+e %= 4;
+console.log(e);
+
+//JavaScript:Javascript is a high-level interpreted, light-weight programming language mainly used to devetop interactive and dynamic web pages. Along with HTML structure) and CSS (styling) is core technology of the world wide web It is used to respond to user actions the (clicka, keypress, form validations) validation forms manipulate html and css dynamically and communicate with server(AP) develop full stack applications with 20 internationally Advantages of javascript Use both the front end and back technologies Easy to Inam for beginners Huge ecosystem (angular node vue,Run in all modem browsers.High demand in jobs and interviews History of Javascript In the year of 1995, Brendan Eich developed First Javascript, a Netscape company within 10 days JavaScript First Name is Mocha, then live script Now is Javascript-markel name .
+
+
+let value_1 = 25 >= 22;
+console.log(value_1);
+
+let value_2 = 12 > 37;
+console.log(value_2);
+
+let value_3 = 6 < 8;
+console.log(value_3);
+
+let result = 15 == '15';
+console.log(result);
+
+let res = 35 ==="35";
+console.log(res);
+
+let not = 6 != "6";
+console.log(not);
