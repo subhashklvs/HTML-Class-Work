79318033df251a81cc943aa9ed63e66495d787c6
subhashklvs <subhashklvs@gmail.com>
2026-04-08 12:18:45 +0530

    updated

--- javascript/10.objectOperations.js
+++ javascript/10.objectOperations.js
@@ -21,8 +21,43 @@ console.log(employeeData.address[0].place);
 
 employeeData.details.salary=60000;
 
-
 employeeData.address[0].place='Hyderabad';
 console.log(employeeData.address[0].place);
 
-console.log(employeeData.address[0].place);
+console.log(employeeData);
+
+delete employeeData.name;
+delete employeeData.details.salary;
+delete employeeData.address[0].house_no;
+
+console.log(employeeData);
+
+console.log(Object.keys(employeeData));
+console.log(Object.values(employeeData));
+console.log(Object.entries(employeeData.details));
+console.log(Object.hasOwn(employeeData.address[0]));
+
+console.log(Object.entries(employeeData));
+console.log("age" in employeeData);
+
+let student={
+    name:'subhashklvs',
+    skills:'python Full Stack Developer',
+    batch_name:'PFS-44',
+    branch_name:'vasantha nagar',
+    greet:function(){
+        console.log("Welcome to Codegnan Your Branch is",this.branch_name);
+    }
+}
+
+function assign(){
+    console.log("Front End Assigned Batches",this.branch_name);
+}
+console.log(student);
+//console.log.call(this.batch_name,"Global Scope");
+student.greet();
+
+assign.apply(student);
+assign.call(student);
+let result=assign.bind(student);
+result();
