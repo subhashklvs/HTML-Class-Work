8930c311c8f6d35a9551a37c3a8914c34dc13609
subhashklvs <subhashklvs@gmail.com>
2026-05-01 12:13:41 +0530

    upadted

--- javascript/25.API.js
+++ javascript/25.API.js
@@ -4,15 +4,15 @@ fetch("https://jsonplaceholder.typicode.com/users")
 .catch(error => console.log(error))
 
 let user={
-    id=11,
+    id  : 11,
     name:"Subhash",
     username:"subhash123",
     email:"subhashklvs@gmail.com",
-    adress:'Chilakaluripet'
+    address:'Chilakaluripet'
 }
 fetch("https://jsonplaceholder.typicode.com/users",{
     method:"POST",
-    hearders:{
+    headers:{
         "Content-Type":"application/json"
     },
     body:JSON.stringify(user)
@@ -21,3 +21,18 @@ fetch("https://jsonplaceholder.typicode.com/users",{
 .then(data => console.log(data))
 .catch(error => console.log(error)) 
     
+let students={
+    name:"lakshmmi venkata sai",
+    age:123,
+    class:"Full stack python"
+}
+fetch("https://jsonplaceholder.8000/students/1",{
+    method:"PUT",
+    headers:{
+        "Content-Type":"application/json"
+    },
+    body:JSON.stringify(students)
+})
+.then(response => response.json())
+.then(data => console.log(data))
+.catch(error => console.log(error))
